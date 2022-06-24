44daf1879359c6750cdb2866aa5348c7309926b1
veronikaDyshnyk <veronika199942@gmail.com>
2022-06-24 12:39:09 +0100

    branch from first branch describing Rick&Morty

--- src/App.js
+++ src/App.js
@@ -2,60 +2,67 @@ import React from "react";
 import CharacterComponent from "./components/CharacterComponent";
 
 function App() {
-    return(
-      <div>
+    return (
+        <div>
 
-          <CharacterComponent
-          name = {'bart'}
-          surname = {'sipmson'}
-          age = {10}
-          info = {'Барт ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі.'}
-          photo = {'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'}
-          />
+            <CharacterComponent
+                id={1}
+                name={'Rick Sanchez"'}
+                status={'Alive'}
+                species={'Human'}
+                gender={'Male'}
+                image={'https://rickandmortyapi.com/api/character/avatar/1.jpeg'}
+            />
+              <CharacterComponent
+                id={2}
+                name={'Morty Smith'}
+                status={'Alive'}
+                species={'Human'}
+                gender={'Male'}
+                image={'https://rickandmortyapi.com/api/character/avatar/2.jpeg'}
+            />
+              <CharacterComponent
+                id={3}
+                name={'Summer Smith'}
+                status={'Alive'}
+                species={'Human'}
+                gender={'Female'}
+                image={'https://rickandmortyapi.com/api/character/avatar/3.jpeg'}
+            />
+              <CharacterComponent
+                id={4}
+                name={'Beth Smith'}
+                status={'Alive'}
+                species={'Human'}
+                gender={'Female'}
+                image={'https://rickandmortyapi.com/api/character/avatar/4.jpeg'}
+            />
+              <CharacterComponent
+                id={5}
+                name={'Jerry Smith'}
+                status={'Alive'}
+                species={'Human'}
+                gender={'Male'}
+                image={'https://rickandmortyapi.com/api/character/avatar/5.jpeg'}
+            />
+              <CharacterComponent
+                id={6}
+                name={'Abadango Cluster Princess'}
+                status={'Alive'}
+                species={'Alien'}
+                gender={'Female'}
+                image={'https://rickandmortyapi.com/api/character/avatar/6.jpeg'}
+            />
 
-          <CharacterComponent
-          name = {'homer'}
-          surname = {'sipmson'}
-          age = {40}
-          info = {'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний.'}
-          photo = {'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}
-          />
-
-          <CharacterComponent
-          name = {'marge'}
-          surname = {'sipmson'}
-          age = {38}
-          info = {'\'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер.'}
-          photo = {'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'}
-          />
-
-          <CharacterComponent
-          name = {'lisa'}
-          surname = {'sipmson'}
-          age = {9}
-          info = {'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.'}
-          photo = {'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'}
-          />
-
-          <CharacterComponent
-          name = {'maggie'}
-          surname = {'sipmson'}
-          age = {1}
-          info = {'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года.'}
-          photo = {'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'}
-          />
 
 
-      </div>
+        </div>
     );
 }
 
 export default App;
 
 
-
-// 1. Описати всю сім'ю сімпсонів (5 персонажів)
-//
 // 2. Створити компонент який описує персонажа (властивості id,name,status,species,gender,image) з цього апі
 // https://rickandmortyapi.com/
 //     https://rickandmortyapi.com/api/character
--- src/components/CharacterComponent.js
+++ src/components/CharacterComponent.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 function CharacterComponent(props) {
-    const {name, surname, age, info, photo} = props;
+    const {id,name,status,species,gender,image} = props;
     return (
         <div>
+            <h2>{id}</h2>
             <h2>{name}</h2>
-            <h2>{surname}</h2>
-            <h3>{age}</h3>
-            <p>{info}</p>
-            <img src={photo} alt={''}/>
+            <h3>{status}</h3>
+            <h3>{species}</h3>
+            <h3>{gender}</h3>
+            <h3>{image}</h3>
+            <img src={image} alt={name}/>
 
         </div>
     );
